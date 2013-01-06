0f241c9cc2280d14aed8b7b72d673cbf63a83236
Josh Bassett <josh.bassett@gmail.com>
2013-01-06 19:06:02 +1100

    Refactored main function.

--- Main.hs
+++ Main.hs
@@ -11,8 +11,8 @@ initState = lift Player.empty >>= World.spawn >>
 loop :: World.WorldState ()
 loop = World.move (fromJust (fromString "acb42012-4d0f-47dd-a541-b6f949f4066c")) >> World.tick
 
-main = do
-  s   <- World.empty
-  s'  <- execStateT initState s
-  s'' <- execStateT loop s'
-  print (show s'')
+main :: IO ()
+main = World.empty >>=
+       execStateT initState >>=
+       execStateT loop >>=
+       print
