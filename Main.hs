9fdea3395207f46fc290cd2548ae6479a34b6869
Josh Bassett <josh.bassett@gmail.com>
2013-01-06 22:54:08 +1100

    Run the loop 10 times.

--- Main.hs
+++ Main.hs
@@ -1,7 +1,9 @@
 import Control.Monad.State
 import Data.Aeson (encode)
+import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Map as Map
 import qualified Player
+import System.Exit
 import qualified World
 import qualified WorldView
 
@@ -10,14 +12,16 @@ initWorld = lift Player.empty >>= World.spawn >>
             lift Player.empty >>= World.spawn
 
 loop :: World.WorldState ()
-loop = do
+loop = forever $ do
   world <- get
+  when (World.age world >= 10) (lift exitSuccess)
   let uuid = head $ Map.keys $ World.players world
   World.move uuid
   World.tick
+  liftIO $ B.putStrLn $ encode $ WorldView.fromWorld world
 
 main :: IO ()
 main = World.empty >>=
        execStateT initWorld >>=
        execStateT loop >>=
-       \world -> print $ encode $ WorldView.fromWorld world
+       print
