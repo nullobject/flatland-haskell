03d5e1e3fe61d65f3f12610cbe92d604c12e88b1
Josh Bassett <josh.bassett@gmail.com>
2013-01-13 21:36:12 +1100

    Updated game to execute queued actions.

--- src/Game.hs
+++ src/Game.hs
@@ -1,44 +1,43 @@
 module Game where
 
 import           Control.Concurrent (threadDelay)
-import           Control.Concurrent.STM (atomically, STM)
+import           Control.Concurrent.STM (atomically)
 import           Control.Monad.State
 import qualified Data.Map as Map
+import           Data.UUID (UUID)
 import           Data.UUID.V4 (nextRandom)
 import           GHC.Conc (unsafeIOToSTM)
 import qualified Player
+import           World (World, WorldState)
 import qualified World
 import qualified WorldView
 import           Types
 
-initWorld :: World.WorldState ()
+initWorld :: WorldState ()
 initWorld = do
   uuid <- liftIO $ nextRandom
   World.spawn $ Player.empty uuid
 
-tick :: World.WorldState ()
-tick = do
-  world <- get
-  let uuid = head $ Map.keys $ World.players world
-  World.move uuid
-  World.tick
+execute :: Action -> UUID -> WorldState ()
+execute Idle uuid = return ()
+execute Move uuid = World.move uuid
 
-requestHandler :: World.World -> Request -> STM ()
-requestHandler world (Request sender action) = do
-  unsafeIOToSTM $ print action
+handleRequest :: World -> Request -> IO World
+handleRequest world (Request sender (ActionMessage action)) = do
+  let uuid = head $ Map.keys $ World.players world
+  putStrLn $ show action ++ " (" ++ show uuid ++ ")"
+  world' <- execStateT (execute action uuid) world
+  let message = WorldViewMessage $ WorldView.fromWorld $ world'
   sender `tell` message
-  where message = WorldViewMessage $ WorldView.fromWorld $ world
+  return world'
+
+tick :: RequestChan -> World -> IO ()
+tick chan world = do
+  threadDelay oneSecond
+  requests <- atomically $ drainTChan chan
+  world' <- execStateT World.tick world
+  world'' <- foldM handleRequest world' requests
+  tick chan world''
 
 run :: RequestChan -> IO ()
-run chan = do
-  world <- execStateT initWorld World.empty
-  run' chan world
-
-run' :: RequestChan -> World.World -> IO ()
-run' chan world = do
-  atomically $ do
-    requests <- drainTChan chan
-    mapM (requestHandler world) requests
-  world' <- execStateT tick world
-  threadDelay oneSecond
-  run' chan world'
+run chan = execStateT initWorld World.empty >>= tick chan
