47afb904dae6cbca54df34f9f15a96be983acecc
Josh Bassett <josh.bassett@gmail.com>
2013-01-15 09:43:55 +1100

    Handle missing header.

--- src/Server.hs
+++ src/Server.hs
@@ -4,28 +4,38 @@ module Server where
 
 import           Control.Concurrent.STM (TChan)
 import           Control.Monad.Trans (liftIO)
-import           Data.Aeson (encode)
-import           Data.ByteString.UTF8 (toString)
+import           Control.Monad.Trans (liftIO)
+import           Data.Aeson (encode, ToJSON)
+import           Data.ByteString.Char8 (unpack)
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Maybe as Maybe
 import           Data.UUID (UUID)
 import qualified Data.UUID as UUID
-import           Network.HTTP.Types (status200)
+import           Network.HTTP.Types (status200, status400)
 import           Network.Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import           Types
 
+returnSuccess :: (Monad m, ToJSON a) => a -> m Response
+returnSuccess value = return $ responseLBS status200 [("Content-Type", "application/json")] $ encode $ value
+
+returnFailure :: Monad m => LBS.ByteString -> m Response
+returnFailure value = return $ responseLBS status400 [("Content-Type", "text/plain")] value
+
 getPlayer :: Request -> IO (Maybe UUID)
 getPlayer request = do
   let player = lookup "X-Player" $ requestHeaders request
-  return $ UUID.fromString $ toString $ Maybe.fromJust player
+  return $ player >>= UUID.fromString . unpack
 
--- FIXME: Error unless the player is given.
 actionHandler :: TChan GameRequest -> Application
 actionHandler chan request = do
-  uuid <- liftIO $ getPlayer request
-  let message = ActionMessage Move $ Maybe.fromJust uuid
-  WorldViewMessage worldView <- liftIO $ chan `ask` message
-  return $ responseLBS status200 [("Content-Type", "application/json")] $ encode $ worldView
+  liftIO $ getPlayer request >>= Maybe.maybe fail ok
+  where
+    ok uuid = do
+      let message = ActionMessage Move uuid
+      WorldViewMessage worldView <- liftIO $ chan `ask` message
+      returnSuccess worldView
+    fail = returnFailure "Missing header X-Player"
 
 application :: TChan GameRequest -> Application
 application chan request =
