d4fd15afc34ed9f937fefaa8d108b11e1b7e3774
Josh Bassett <josh.bassett@gmail.com>
2013-02-02 18:09:12 +1100

    Flipped the RequestChannel type parameters.

--- src/Message.hs
+++ src/Message.hs
@@ -7,15 +7,15 @@ import Identifier
 type Message = (Identifier, Action)
 
 data Request a b = Request {
-  payload :: b,
-  sender  :: Response a
+  payload :: a,
+  sender  :: Response b
 }
 
-type Response a = TMVar a
+type Response b = TMVar b
 
 type RequestChannel a b = TChan (Request a b)
 
-drainTChan :: TChan a -> STM [a]
+drainTChan :: TChan b -> STM [b]
 drainTChan chan = do
   empty <- isEmptyTChan chan
   if empty then return [] else drainTChan'
@@ -29,13 +29,13 @@ drainTChan chan = do
 drain :: RequestChannel a b -> IO [Request a b]
 drain chan = atomically $ drainTChan chan
 
--- Replies to a message.
-tell :: Response a -> a -> IO ()
-tell sender message = atomically $ putTMVar sender message
+-- Replies to a request.
+tell :: Response b -> b -> IO ()
+tell sender payload = atomically $ putTMVar sender payload
 
--- Writes a message to the channel and waits for a response.
-ask :: RequestChannel a b -> b -> IO a
-ask chan message = do
+-- Writes a request to the channel and waits for a response.
+ask :: RequestChannel a b -> a -> IO b
+ask chan payload = do
   sender <- newEmptyTMVarIO
-  atomically $ writeTChan chan $ Request message sender
+  atomically $ writeTChan chan $ Request payload sender
   atomically $ takeTMVar sender
