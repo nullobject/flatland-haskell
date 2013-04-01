3007f12f6458190df323fd8b285118645a65055b
Josh Bassett <josh.bassett@gmail.com>
2013-04-01 21:09:33 +1100

    Added tiles.

--- static/flatland.js
+++ static/flatland.js
@@ -1,35 +1,29 @@
 (function () {
   var grid = {
-    renderGrid: function (tiles, size) {
-      var scale = 20;
-
-      d3
-      .select("#grid")
-      .selectAll("li")
-      .data(tiles)
-      .enter()
-      .append("li")
-      .style("left", function (d, i) { return (i % size) * scale + "px"; })
-      .style("top",  function (d, i) { return Math.floor(i / size) * scale + "px"; })
-      .style("width",  scale + "px")
-      .style("height", scale + "px")
-      .attr("class", function (d, i) { return "tile-" + (d & 0xf); });
+    renderGrid: function (tiles, size, scale) {
+      d3.select("#grid")
+        .selectAll("li")
+        .data(tiles)
+        .style("left", function (d, i) { return (i % size) * scale + "px"; })
+        .style("top",  function (d, i) { return Math.floor(i / size) * scale + "px"; })
+        .style("width",  scale + "px")
+        .style("height", scale + "px")
+        .attr("class", function (d, i) { return "tile-" + (d & 0xf); })
+        .enter()
+        .append("li");
     },
 
-    renderEntities: function (entities, size) {
-      var scale = 20;
-
-      d3
-      .select("main")
-      .selectAll("div")
-      .data(entities)
-      .attr("class", "entity")
-      .style("left",  function (entity) { return (entity.position[0] - 0.5 + (size / 2)) * scale + "px"; })
-      .style("top", function (entity) { return (entity.position[1] - 0.5 + (size / 2)) * scale + "px"; })
-      .style("width",  20 + "px")
-      .style("height", 20 + "px")
-      .enter()
-      .append("div")
+    renderEntities: function (entities, size, scale) {
+      d3.select("main")
+        .selectAll("div")
+        .data(entities)
+        .attr("class", "entity")
+        .style("left",  function (entity) { return (entity.position[0] - 0.5 + (size / 2)) * scale + "px"; })
+        .style("top", function (entity) { return (entity.position[1] - 0.5 + (size / 2)) * scale + "px"; })
+        .style("width",  scale + "px")
+        .style("height", scale + "px")
+        .enter()
+        .append("div");
     }
   };
 
@@ -45,15 +39,10 @@
 
   source.onmessage = function(message) {
     var data = JSON.parse(message.data);
+    var size = Math.sqrt(data.tiles.length), scale = 16;
+    var entities = data.players.map(function (player) { return player.entity; });
 
-    // document.getElementsByTagName("main")[0].innerHTML = data.age;
-
-    var size = Math.sqrt(data.tiles.length);
-    grid.renderGrid(data.tiles, size);
-
-    var entities = data.players.map(function (player) {
-      return player.entity;
-    });
-    grid.renderEntities(entities, size);
+    grid.renderGrid(data.tiles, size, scale);
+    grid.renderEntities(entities, size, scale);
   }
 }());
