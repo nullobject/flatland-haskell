d40e19c5fcee219163148ab4deb01421e4d93bb9
Josh Bassett <josh.bassett@gmail.com>
2013-04-12 21:40:25 +1000

    Bullet cleanups.

--- src/Bullet.hs
+++ src/Bullet.hs
@@ -1,15 +1,14 @@
 module Bullet where
 
-import Geometry (Position, Velocity)
+import Geometry
 import Data.Aeson
 
 -- An bullet is an actor in the world.
 data Bullet = Bullet
-  { bulletPosition  :: Position
-  , bulletVelocity  :: Velocity
+  { bulletPosition :: Position
+  , bulletVelocity :: Velocity
   } deriving (Show)
 
 instance ToJSON Bullet where
   toJSON bullet = object [ "position" .= bulletPosition bullet
                          , "velocity" .= bulletVelocity bullet ]
-
